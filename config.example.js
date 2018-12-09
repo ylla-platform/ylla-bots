782a8b4e37dbc1295e581766cd3f851d2b32f577
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2018-12-09 14:56:08 +0100

    Initial commit

---
+++ config.example.js
@@ -0,0 +1,12 @@
+module.exports = {
+    git: {
+        secret: 'your-github-web-hook-secret',
+        reponame: 'testrepo',
+        host: '0.0.0.0',
+        port: 3420
+    },
+
+    telegram: {
+        token: 'your-telegram-bot-api-token'
+    }
+};
